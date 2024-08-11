ed802160a1fd343024423456be9615655eff319a
xukeawsl <xukeawsl@gmail.com>
2024-08-11 17:03:51 +0800

    update example

--- example/echo/svc_echo.cpp
+++ example/echo/svc_echo.cpp
@@ -8,6 +8,7 @@ extern "C" void svc_echo(coro_actor_pack_t* rqst, coro_actor_pack_t* resp) {
 
 asio::awaitable<void> svc_echo_impl(coro_actor_pack_t* rqst, coro_actor_pack_t* resp) {
     SPDLOG_INFO("call svc_echo_impl");
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     resp->type = rqst->type;
     resp->value = std::move(rqst->value);
     g_actor_ctx->notify_complete();
